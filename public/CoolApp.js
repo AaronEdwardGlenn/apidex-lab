ce1870bd5df96ae46d700552be104db3137d98a2
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2019-11-11 10:33:29 -0800

    reset master

--- public/CoolApp.js
+++
@@ -1,5 +0,0 @@
-import Cool from './app.js'; 
-
-const app = new Cool(); 
-const dom = app.renderDOM(); 
-document.body.prepend(dom); 
--- public/pokedex/index.js
+++
@@ -1,5 +0,0 @@
-import App from './pokedexapp.js';
-
-const app = new App();
-const dom = app.renderDOM();
-document.body.prepend(dom);
